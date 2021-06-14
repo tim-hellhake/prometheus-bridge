7553195115aeae9b9f6831d4888260ab34bb2951
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-06-14 23:15:22 +0200

    Add newlines after help/type

--- src/prometheus-bridge.ts
+++ src/prometheus-bridge.ts
@@ -82,19 +82,19 @@ export class PrometheusBridge extends Adapter {
       for (const [deviceId, { title, lastUpdate, properties }] of Object.entries(this.entries)) {
         const diff = (new Date().getTime() - lastUpdate.getTime()) / 1000;
         // eslint-disable-next-line max-len
-        response += '# HELP last_device_update Time since the last update of the device';
-        response += '# TYPE last_device_update gauge';
+        response += '# HELP last_device_update Time since the last update of the device\n';
+        response += '# TYPE last_device_update gauge\n';
         response += `last_device_update{deviceId="${deviceId}", deviceTitle="${title}"} ${diff}\n`;
 
         for (const [property, { value, lastUpdate }] of Object.entries(properties)) {
           // eslint-disable-next-line max-len
-          response += `# HELP ${property} ${property} property of the device ${title} (${deviceId})`;
-          response += `# TYPE ${property} gauge`;
+          response += `# HELP ${property} ${property} property of the device ${title} (${deviceId})\n`;
+          response += `# TYPE ${property} gauge\n`;
           response += `${property}{deviceId="${deviceId}", deviceTitle="${title}"} ${value}\n`;
 
           const diff = (new Date().getTime() - lastUpdate.getTime()) / 1000;
-          response += '# HELP last_property_update Time since the last update of the property';
-          response += '# TYPE last_property_update gauge';
+          response += '# HELP last_property_update Time since the last update of the property\n';
+          response += '# TYPE last_property_update gauge\n';
           // eslint-disable-next-line max-len
           response += `last_property_update{deviceId="${deviceId}", deviceTitle="${title}", property="${property}"} ${diff}\n`;
         }
